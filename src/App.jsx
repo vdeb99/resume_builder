ca27ada3093ac8232cd831a7810a3c6c4fce5589
Prantick Deb <prantickdeb2005@gmail.com>
2025-09-21 15:41:58 +0530

    Router check

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import { Outlet } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Dialog.Root>
-        <Dialog.Trigger className="px-4 py-2 bg-blue-600 text-white rounded">
-          Open Modal
-        </Dialog.Trigger>
-        <Dialog.Overlay className="fixed inset-0 bg-black/40" />
-        <Dialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white p-6 rounded shadow-lg w-80">
-          <Dialog.Title className="text-lg font-bold">Hello Radix!</Dialog.Title>
-          <Dialog.Description className="mt-2 text-gray-600">
-            This modal is powered by Radix + Tailwind v4.
-          </Dialog.Description>
-          <Dialog.Close className="mt-4 px-3 py-1 bg-gray-200 rounded">
-            Close
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Root>
-    </div>
+   <>
+    <Outlet />
+   </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {SignIn} from './Auth/index.auth.js'
+import {Home,CreateResume,Dashboard} from './Pages/index.pages.js'
 
+const router=createBrowserRouter([
+  {
+    element:<App/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/dashboard',
+        element:<Dashboard/>
+      },
+      {
+        path:'/create-resume',
+        element:<CreateResume/>
+      }
+
+    ]
+    
+  },
+  {
+    path:'/sign-in',
+    element:<SignIn/>
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
